cd1115f9d5a953f086cb12f6860b883a725b0f35
Aman Srivastav <30969315+sriaman@users.noreply.github.com>
2018-02-15 22:48:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,67 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var myfile={
+    title:'startup alumni meet',
+    heading:'JERC ALUMNI MEET STARTUP',
+    contents:` <p>
+                    in jecrc aliumni startup meet was happen where lots of alumni's are in the college and share their 
+                    startup's and four of them are <b>RAS</b> qualifiers.
+                </p>
+ <p>
+                    one of the best thing is that they all are good motivaters and do there startup's 
+                    by self motivating.
+                </p>
+   <dl>
+                    <dt>Arpit Agrawal</dt>
+                    <dd>-director of JECRC</dd>
+                    <dt>
+                        DR.V.k. chandana
+                    </dt>
+                    <dd>-principal of JECRC</dd>
+                </dl>
+                <div>
+                <p>
+                    And many other Guests are  attended.
+                </p>
+                </div>`
+};
+function createtemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var contents=data.heading;
+
+var htmlTemplate=`<!DOCTYPE html>
+<html>
+    <head>
+        <title>${title}</title>
+        <meta name="viewport" content="width=device-width initial-scale=1">
+        <link href="/ui/style.css" rel="stylesheet" />
+        </head>  
+        <body>
+            <a  href="/">Home</a>
+            <hr>
+            <div class="contents">
+            <h2>${heading}</h2>
+            <div>
+                  ${contents}
+            
+                
+            </div>
+            </div>
+        </body>
+</html>
+
+`;
+    return htmltemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
 app.get('/newfile',function(req,res){
- res.sendFile(path.join(__dirname, 'ui', 'newfile.html'));
+ res.send(createtemplate(myfile));
 });
 app.get('/',function(req,res){
     res.sendFile('this is the second article');
